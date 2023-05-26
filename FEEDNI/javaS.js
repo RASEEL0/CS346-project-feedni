2e770b3ee4211b76bb8de95b8d708ad249baa245
Raseel <106612674+RASEEL0@users.noreply.github.com>
2023-05-27 01:06:09 +0300

    Update javaS.js

--- FEEDNI/javaS.js
+++ FEEDNI/javaS.js
@@ -5,3 +5,10 @@ let sidebar =document.querySelector('.sidebar');
 btn.onclick=function(){
     sidebar.classList.toggle('active');
 };
+
+/*
+pages that allow user to add content 
+*/
+if (!checkLoginState()) {
+    localStorage.setItem("previousPage", window.location.href);
+  }
