d662886cb92f84d9a3ea66e701a985f68ee0e5bb
Raseel <106612674+RASEEL0@users.noreply.github.com>
2023-05-29 04:00:15 +0300

    Update javaA.js
    update users register conditions for user names and password .check if unique user name and some password constraint .

--- FEEDNI/javaA.js
+++ FEEDNI/javaA.js
@@ -90,6 +90,7 @@ const valid = users.some(user => {
 });
 
 //***************************REGISTER*********************************************** */
+
 const regForm = document.getElementById("register-form");
 
 regForm.addEventListener("submit", register);
@@ -107,21 +108,133 @@ function register(event) {
        document.getElementById("repeatpassword-error").textContent = "Passwords do not match!";
      return;  
     }
- //after regester pushing user information  into users matrix
+    //code for input constraint (condition )
+
+ //after regester pushing user information  into users matrix befor desgin database
    users.push({
      username: regUsername,
      password: regPassword,
      email: email
    });
- //then write message after succseful 
- alert("Registration successful!");
+   //***********after data base 
+ const mongoose = require("mongoose");
+
+// Connect to MongoDB change names leater
+mongoose.connect("mongodb://localhost/userdatabase");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+   
+   function checkPassword(password) {
+      const lowerCaseLetters = /[a-z]/g;
+      const upperCaseLetters = /[A-Z]/g;
+      const numbers = /[0-9]/g;
+      const specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    
+      if(password.length < 8) {
+        return false;
+      }
+    
+      if(!password.match(lowerCaseLetters)) {
+        return false;
+      }
+    
+      if(!password.match(upperCaseLetters)) {
+        return false;  
+      }
+    
+      if(!password.match(numbers)) {
+        return false;    
+      }
+    
+      if(!password.match(specialChars)) {
+        return false;
+      }
+    
+      return true;
+    }
+    
+    function checkUsername(username) {
+      if(username.length < 3 || username.length > 15) {
+        return false;   
+      }
+    
+      const regex = /^[a-zA-Z0-9_]+$/;
+      if(!regex.test(username)) {
+        return false;    
+      }
+    
+      return true;
+    }
+  
+    function usernameExists(username) {
+       return User.findOne({ username }).then(user => !!user);
+  }//end check database
+    //to hash password
+    function hashPassword(password) {
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(password, salt);
+      return hash;
+    }
+    
+    function saveUser(username, password) {
+      const user = new User({
+        username, 
+        password: hashedPassword
+      });
+      
+      user.save();
+    }
+    function register(username, password) {
+      if (usernameExists(username)) {
+        return "Username exists!";  
+      }
+      
+      if (!checkPassword(password)) {
+        return "Invalid password!";  
+      } 
+      
+      if (!checkUsername(username)) {
+        return "Invalid username!"; 
+      }
+      const hashedPassword = hashPassword(password);
+  
+  saveUser(username, hashedPassword);
+  
+  return "User created!";
+}
+//***********call function 
+const result = register("john", "password123");
+
+// Show result
+if (result === "User created!") {
+  // Clear fields
+  alert("Registration successful!");
  document.getElementById("Fname").value = "";
  document.getElementById("Lname").value = "";
  document.getElementById("regUserName").value = "";  
  document.getElementById("regPassword").value = "";
  document.getElementById("email").value = "";
  document.getElementById("regRepeatPassword").value = "";
- }
+ 
+} else {
+  // Show error
+  const errorElement = document.getElementById("register-error");
+  
+  if (result === "Username exists!") {
+    errorElement.textContent = "Username already exists!";   
+  } else if (result === "Invalid password!") {
+    errorElement.textContent = "Invalid password!";  
+  } else if (result === "Invalid username!") {
+    errorElement.textContent = "Invalid username!";  
+  }  
+}
+}
 //***************************LOGOUT*********************************************** */
  function logout() {
    localStorage.removeItem("isLoggedIn");
